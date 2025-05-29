21a2e6967e6f4f277469b64d026868f3782c60c6
tyanfarm <phamquangtuyen.nt@gmail.com>
2025-05-29 12:58:41 +0700

    update

--- train.py
+++ train.py
@@ -147,7 +147,7 @@ WEBHOOK_URL = "https://discord.com/api/webhooks/1377329996816711751/Syut1WtRiT5K
 result_best = 'Best test hr: {}, ndcg: {} at round {}'.format(hit_ratio_list[final_test_round],
                                                              ndcg_list[final_test_round],
                                                              final_test_round)
-str += '\n' + f"test hr list: {hit_ratio_list}" + '\n' + f"test ndcg list: {ndcg_list}"
+# str += '\n' + f"test hr list: {hit_ratio_list}" + '\n' + f"test ndcg list: {ndcg_list}"
 message_discord = f"\n**Dataset: {config['dataset']}**\n```{str}\n{result_best}```\n"
 send_webhook_message(WEBHOOK_URL, message_discord, username="Notification Bot")
 
